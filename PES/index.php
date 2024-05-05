53fd0cab888d5c0b42e90152458472075f1888c0
NeilDenzel <neildenzel1612q@gmail.com>
2024-05-05 16:59:57 -0500

    PES change

--- PES/index.php
+++ PES/index.php
@@ -2,8 +2,22 @@
     include("../componentes/Header.php");
 ?>
 
-  
+<div class="body">
+    <div>
+    <?php
+        include("../componentes/Nav.php");
+    ?>
+    </div>
+    <article>
+        escribe cualquier wbda aqui
+    </article>
+    <div>
+    <?php
+        include("../componentes/Aside.php");
+    ?>
+    </div>
+</div>
+
 <?php
-    include("../componentes/Footer.php");
+    include("../componentes/Footer.php")
 ?>
-    
